9154756f44ee584b34633ae0053f7403db695cde
i6973-er <65159840+i6973-er@users.noreply.github.com>
2020-11-09 14:35:33 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Color-change
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,24 @@
+//calculating time
+let h = hour();
+text('Current hour:\n' + h ,5,50);
+
+let m = minute();
+text('Current minute: \n' + m, 5, 50);
+
+let s = second();
+text('Current second: \n' + s, 5, 50);
+
+function setup() {
+  createCanvas(720, 400);
+  noStroke();
+}
+
+function draw() {
+  background(0);
+  // Scale the mouseX value from 0 to 720 to a range between 0 and 175
+  let c = map(mouseX, 0, width, 0, 175);
+  // Scale the mouseX value from 0 to 720 to a range between 40 and 300
+  let d = map(mouseX, 0, width, 40, 300);
+  fill(255, c, 0);
+  ellipse(width/2, height/2, d, d);
+}
